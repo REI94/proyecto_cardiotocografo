d29f8d9be67419b3c6a6e4b35122da0075357c6a
José Reinoza <josemrd1994@gmail.com>
2019-07-06 23:39:33 -0430

    1.1 - 5 Codificación del modelo Historia
    Se codificó el modelo Historia en models.py de la aplicación
    Cardiotocografo con los campos: codigo, enfermedad_padecida y
    enfermedad_hereditarias. Se incluyeron metadatos, aunque de momento no
    cambian el orden de los registros, pueden ser útiles posteriormente
    cuando se puedan incluir los modelos Persona, Paciente y Alergia.
    
    Además, se agregó el método estandar __str__(self) y get_absolute_url(self) parapoder manejar las instancias de este modelo.

--- Clínica/Clínica/settings.py
+++ Clínica/Clínica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Cardiotocografo',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Clínica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Clinica',
+        'USER': 'postgres',
+        'PASSWORD': 'joserei02',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ve'
 
 TIME_ZONE = 'UTC'
 
--- Clínica/apps/Cardiotocografo/models.py
+++ Clínica/apps/Cardiotocografo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Historia(models.Model):
+
+    #-----Campos-----
+    codigo = models.AutoField('Código', primary_key = True)
+    #paciente   Ignorado, falta el modelo Paciente
+    #familiar_emr    Ignorado, falta el modelo Persona
+    #alergia    Ignorado, falta el modelo Alergia
+    enfermedad_padecida = models.CharField('Enfermedad padecida', max_length = 200, blank = False, null = False)
+    enfermedad_hereditarias = models.TextField('Enfermedades hereditarias', blank = True)
+
+    #-----Metadata-----
+    class Meta:
+        ordering = ["codigo", "enfermedad_padecida", "enfermedad_hereditarias"]
+
+    #-----Métodos-----
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.codigo)])
+
+    def __str__(self):
+        return self.codigo
